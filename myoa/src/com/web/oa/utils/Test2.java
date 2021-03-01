4941d8f56b356d6930f3de2c99b9d74eff594825
smiles365 <1052639169@qq.com>
2021-03-01 16:18:15 +0800

    修改了Test2

--- myoa/src/com/web/oa/utils/Test2.java
+++ myoa/src/com/web/oa/utils/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("44455555666");
+		System.out.println("11111111111");
 	}
 
 }
